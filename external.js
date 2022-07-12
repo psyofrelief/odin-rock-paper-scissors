df1fbac68e859ede7c0e0665230552b1d8f8c4a5
psyofrelief <107062781+psyofrelief@users.noreply.github.com>
2022-07-12 15:08:54 +0800

    Declares function that plays 1 round, and one in case  user input invalid

--- external.js
+++ external.js
@@ -1,3 +1,4 @@
+// allows computer to return random choice
 function computerPlay() {
   const choices = ["Rock", "Paper", "Scissors"];
   // sets index to number between 0, 1
@@ -9,40 +10,87 @@ function computerPlay() {
   return choices[index];
 }
 
+// allows user and computer to play 1 round of RPScissors
 function singleRound(playerSelection, computerSelection) {
+  //   assigns prompt dialogue box to `playerSelection`
   playerSelection = prompt("Select 'Rock', 'Paper', or 'Scissors'");
+  //   assigns random choice from computerPlay() to computerSelection
+  computerSelection = computerPlay();
+  const playerRegex = /^Rock$|^Paper$|^Scissors$/i;
+  //   assigns boolean result of user input's regex test to `input`
+  let input = playerRegex.test(playerSelection);
+  // defines win variable, prior to it having a value
+  let win = null;
+
+  // defines what is considered as a win
+  if (
+    // if input regex is true and if playerSelection > computerSelection
+    input &&
+    ((playerSelection === "rock" &&
+      computerSelection === "Scissors") ||
+      (playerSelection === "paper" && computerSelection === "Rock") ||
+      (playerSelection === "scissors" &&
+        computerSelection === "Paper"))
+  ) {
+    win = true;
+  }
+
+  // defines a variable that makes the user's input title case
+  let playerSelectionUpper =
+    // transforms first letter to uppercase
+    playerSelection[0].toUpperCase() +
+    // transforms second to last letters to lowercase
+    playerSelection.slice(1).toLowerCase();
+
+  // defines condition - if win = true
+  if (win) {
+    return `${playerSelectionUpper} beats ${computerSelection}. You win!`;
+    // defines condition - if both playerSelection and computerSelection in lowercase are ===
+  } else if (
+    computerSelection.toLowerCase() == playerSelection.toLowerCase()
+  ) {
+    return `${playerSelectionUpper} matches evenly with ${computerSelection}. It's a draw!`;
+    // defines condition - if input doesn't pass regex test
+  } else if (!input) {
+    console.log(singleRoundError());
+    //   defines condition - if win = false
+  } else {
+    return `${computerSelection} beats ${playerSelectionUpper}. You lose!`;
+  }
+}
+
+// declares function to be called when invalid user input (invalid regex)
+function singleRoundError(playerSelection, computerSelection) {
+  playerSelection = prompt("Pick ROCK, PAPER, or SCISSORS.");
   computerSelection = computerPlay();
   const playerRegex = /^Rock$|^Paper$|^Scissors$/i;
   let input = playerRegex.test(playerSelection);
-
   let win = false;
 
   if (
-    (input &&
-      playerSelection === "rock" &&
+    input &&
+    ((playerSelection === "rock" &&
       computerSelection === "Scissors") ||
-    (playerSelection === "paper" && computerSelection === "Rock") ||
-    (playerSelection === "scissors" && computerSelection === "Paper")
+      (playerSelection === "paper" && computerSelection === "Rock") ||
+      (playerSelection === "scissors" &&
+        computerSelection === "Paper"))
   ) {
     win = true;
-  } else {
-    win = false;
   }
 
-  if (win) {
-    console.log(
-      `${playerSelection} beats ${computerSelection}. You win!`
-    );
+  let playerSelectionUpper =
+    playerSelection[0].toUpperCase() +
+    playerSelection.slice(1).toLowerCase();
+
+  if (win && input) {
+    return `${playerSelectionUpper} beats ${computerSelection}. You win!`;
   } else if (
-    computerSelection.toLowerCase() === playerSelection.toLowerCase()
+    computerSelection.toLowerCase() == playerSelection.toLowerCase()
   ) {
-    console.log("It's a draw!");
+    return `${playerSelectionUpper} matches evenly with ${computerSelection}. It's a draw!`;
+  } else if (!input) {
+    singleRoundError();
   } else {
-    console.log(
-      `${computerSelection} beats ${playerSelection}. You lose!`
-    );
+    return `${computerSelection} beats ${playerSelectionUpper}. You lose!`;
   }
-
-  console.log(playerSelection);
-  console.log(computerSelection);
 }
